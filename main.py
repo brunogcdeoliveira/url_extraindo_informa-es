730f7ec20be85e985cb388fa3c930485365c39b4
Bruno de Oliveira <b.oliveirasud@gmail.com>
2022-04-29 08:47:09 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# url = 'http://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100'
+url = ' '
+
+# Sanitização da URL
+url = url.strip()
+
+# Validação da URL
+if url == '':
+    raise ValueError('A URL está vazia.')
+
+# Separa a base e os parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[(indice_interrogacao+1):]
+print(url_parametros)
+
+# Busca o valor de um parâmetro
+# paramentros: moedaDestino // moedaOrigem  // quantidade
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
+
